6aef4ef605c7073c76908c8b4e5fdd3e3b8cd29f
Arthur Ribeiro <devarthurribeiro@gmail.com>
2018-09-05 14:06:56 -0300

    add script to capture screen

--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,34 @@
 const { desktopCapturer } = require('electron')
+  
+desktopCapturer.getSources({types: ['window', 'screen']}, (error, sources) => {
+  if (error) throw error
+  for (let i = 0; i < sources.length; ++i) {
+      navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          mandatory: {
+            chromeMediaSource: 'desktop',
+            chromeMediaSourceId: sources[i].id,
+            minWidth: 1280,
+            maxWidth: 1280,
+            minHeight: 720,
+            maxHeight: 720
+          }
+        }
+      })
+      .then((stream) => handleStream(stream))
+      .catch((e) => handleError(e))
+      return
+  }
+})
 
+function handleStream (stream) {
+  const video = document.querySelector('video')
+  video.srcObject = stream
+  console.log('ok');
+  video.onloadedmetadata = (e) => video.play()
+}
 
-console.log(desktopCapturer);
+function handleError (e) {
+  console.log(e)
+}
